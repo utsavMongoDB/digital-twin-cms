95d39bf2121e574b8c032a25cce59710e97a92a5
Utsav Talwar <114057324+utsavMongoDB@users.noreply.github.com>
2024-05-06 16:02:42 +0530

    Update setup.sh

--- cdk/setup.sh
+++ cdk/setup.sh
@@ -71,22 +71,56 @@ echo "Please enter the following details to setup AWS : "
 sagemakerEndpoint=$SAGEMAKER_ENDPOINT
 mongoURI=$MONGODB_URI
 
-pwd
-cd ../aws-sagemaker
-## Create AWS ECR Repository 
-echo "Creating ECR Repositories..."
+## Create AWS Eventbus 
+echo "Associating Eventbus..."
+
+# #Get the trigger ID 
+# read -p "Enter the trigger ID: " triggerId
+# Login to the MongoDB Cloud API
+curl_output=$(curl -s --request POST \
+  --header 'Content-Type: application/json' \
+  --header 'Accept: application/json' \
+  --data '{"username": '$API_KEY', "apiKey": '$PRIVATE_KEY'}' \
+  https://services.cloud.mongodb.com/api/admin/v3.0/auth/providers/mongodb-cloud/login)
+
+# Extract the access_token using jq
+access_token=$(echo "$curl_output" | jq -r '.access_token')
+
+# appservices login --api-key=aeewzbjo --private-api-key=20cd1330-cfb4-4ddc-8c96-49ffb3cee1be
+# Save in a variable
+appservices_output=$(appservices apps list)
+echo "$appservices_output" > appservices_output.log 
+read -r project_id client_app_id <<< "$(awk '/connected-vehicle-xnbjimf/ {print $2, $3}' appservices_output.log)"
+echo "Client App ID: $client_app_id"
+echo "Project ID: $project_id"
+
+
+curl_output=$(curl -s --request GET \
+  --header "Authorization: Bearer $access_token" \
+  "https://services.cloud.mongodb.com/api/admin/v3.0/groups/$project_id/apps/$client_app_id/triggers")
+echo "Curl output: $curl_output"
+triggerId=$(echo "$curl_output" | jq -r '.[] | select(.name == "eventbridge_publish_battery_telemetry") | ._id')
+
+aws events create-event-bus --region $awsRegion --event-source-name aws.partner/mongodb.com/stitch.trigger/$triggerId --name aws.partner/mongodb.com/stitch.trigger/$triggerId 
+
+echo "Associated!"
+
+# pwd
+# cd ../aws-sagemaker
+# ## Create AWS ECR Repository 
+# echo "Creating ECR Repositories..."
 # aws ecr create-repository --repository-name cli_connected_vehicle_atlas_to_sagemaker --region $awsRegion
 # aws ecr create-repository --repository-name cli_connected_vehicle_sagemaker_to_atlas --region $awsRegion
 # echo "ECR Repositories created for storing Lambda functions!"
 
-cd code/pull_from_mdb
-## Update the Sagemaker Endpoint and Eventbus
-sed -i "s/<SAGEMAKER_ENDPOINT>/$sagemakerEndpoint/" app.py
-sed -i "s/<REGION>/$awsRegion/" app.py
+# cd code/pull_from_mdb
+# ## Update the Sagemaker Endpoint and Eventbus
+# sed -i "s/<SAGEMAKER_ENDPOINT>/$sagemakerEndpoint/" app.py
+# sed -i "s/<REGION>/$awsRegion/" app.py
 
-echo "Building and pushing the image to ECR..."
+# echo "Building and pushing the image to ECR..."
 
-docker login -u AWS -p $(aws ecr get-login-password --region $awsRegion) $awsID.dkr.ecr.$awsRegion.amazonaws.com
-docker build -t cli_connected_vehicle_atlas_to_sagemaker .
-docker tag cli_connected_vehicle_atlas_to_sagemaker:latest $awsID.dkr.ecr.$awsRegion.amazonaws.com/cli_connected_vehicle_atlas_to_sagemaker:latest
-docker push $awsID.dkr.ecr.$awsRegion.amazonaws.com/cli_connected_vehicle_atlas_to_sagemaker:latest
+# docker login -u AWS -p $(aws ecr get-login-password --region $awsRegion) $awsID.dkr.ecr.$awsRegion.amazonaws.com
+# docker build -t cli_connected_vehicle_atlas_to_sagemaker .
+# docker tag cli_connected_vehicle_atlas_to_sagemaker:latest $awsID.dkr.ecr.$awsRegion.amazonaws.com/cli_connected_vehicle_atlas_to_sagemaker:latest
+# docker push $awsID.dkr.ecr.$awsRegion.amazonaws.com/cli_connected_vehicle_atlas_to_sagemaker:latest
