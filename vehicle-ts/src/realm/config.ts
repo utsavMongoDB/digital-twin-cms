f93fb616efec47529e80978dcc95162a26c05349
Utsav Talwar <114057324+utsavMongoDB@users.noreply.github.com>
2024-05-06 13:06:33 +0530

    Update config.ts

--- vehicle-ts/src/realm/config.ts
+++ vehicle-ts/src/realm/config.ts
@@ -1,6 +1,6 @@
 
 // Provide the Realm App ID
-export const appID = "<--YOUR APP SERVICES ID-->"
+export const appID = "connected-vehicle-.*"
 
 // Provide the configured Email/Password user account. 
 // If you have changed the password while creating the user you must update it here too
