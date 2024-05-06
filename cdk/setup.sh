1caebc4bd8fe4f6d2ef119204c61584650caa03a
Utsav Talwar <114057324+utsavMongoDB@users.noreply.github.com>
2024-05-06 16:23:44 +0530

    Update setup.sh

--- cdk/setup.sh
+++ cdk/setup.sh
@@ -25,41 +25,41 @@ awsRegion=$AWS_REGION
 echo "---------------------- MONGODB ATLAS SETUP ----------------------------"
 npm install -g atlas-app-services-cli
 
-# # Update AWS Account ID
-# cd ../atlas-backend/Connected-Vehicle/triggers
-# sed -i "s/<ACCOUNT_ID>/979559056307/" eventbridge_publish_battery_telemetry.json
-# sed -i "s/<REGION>/us-east-1/" eventbridge_publish_battery_telemetry.json
-# # rm eventbridge_publish_battery_telemetry.json1  
+# Update AWS Account ID
+cd ../atlas-backend/Connected-Vehicle/triggers
+sed -i "s/<ACCOUNT_ID>/979559056307/" eventbridge_publish_battery_telemetry.json
+sed -i "s/<REGION>/us-east-1/" eventbridge_publish_battery_telemetry.json
+# rm eventbridge_publish_battery_telemetry.json1  
 
 
-# cd ../../
-# echo "Logging in to Atlas..."
+cd ../../
+echo "Logging in to Atlas..."
 
-# echo "Pushing Connected-Vehicle app to Atlas...This may take a while!"
-# #TODO: Fix this for Appservices CLI
+echo "Pushing Connected-Vehicle app to Atlas...This may take a while!"
+#TODO: Fix this for Appservices CLI
 
-# app_id_info=$(appservices push --local ./Connected-Vehicle --remote Connected-Vehicle -y | tee output.log | tail -1)
-# app_id=$(echo $app_id_info | cut -d' ' -f 5)
+app_id_info=$(appservices push --local ./Connected-Vehicle --remote Connected-Vehicle -y | tee output.log | tail -1)
+app_id=$(echo $app_id_info | cut -d' ' -f 5)
 
-# echo "-------------------------------------------------"
-# echo "|         App Id: $app_id    |"
-# echo "-------------------------------------------------"
+echo "-------------------------------------------------"
+echo "|         App Id: $app_id    |"
+echo "-------------------------------------------------"
 
-# echo "Creating a user for the app..."
-# appservices users create --type email --email demo --password demopw --app $app_id
+echo "Creating a user for the app..."
+appservices users create --type email --email demo --password demopw --app $app_id
 
-# echo "Below are the apps in your account..."
-# appservices apps list
+echo "Below are the apps in your account..."
+appservices apps list
 
-# cd ../vehicle-ts/src/realm
-# sed -i "s/connected-vehicle-.*/"$app_id"\"/" config.ts
-# cd ../..
-# npm install
-# npm run build
-# echo "Starting the web-app..."
-# npm start
-# # nohup npm start > start.log 2>&1 &
-# echo "Web-app started successfully!"
+cd ../vehicle-ts/src/realm
+sed -i "s/connected-vehicle-.*/"$app_id"\"/" config.ts
+cd ../..
+npm install
+npm run build
+echo "Starting the web-app..."
+npm start
+# nohup npm start > start.log 2>&1 &
+echo "Web-app started successfully!"
 
 echo "---------------------- AWS SETUP ----------------------------"
 
@@ -72,8 +72,7 @@ mongoURI=$MONGODB_URI
 ## Create AWS Eventbus 
 echo "Associating Eventbus..."
 
-# #Get the trigger ID 
-# read -p "Enter the trigger ID: " triggerId
+# Get the trigger ID 
 # Login to the MongoDB Cloud API
 curl_output=$(curl -s --request POST \
   --header 'Content-Type: application/json' \
@@ -93,7 +92,7 @@ read -r project_id client_app_id <<< "$(awk '/connected-vehicle-xnbjimf/ {print 
 echo "Client App ID: $client_app_id"
 echo "Project ID: $project_id"
 
-
+# Get Trigger ID
 curl_output=$(curl -s --request GET \
   --header "Authorization: Bearer $access_token" \
   "https://services.cloud.mongodb.com/api/admin/v3.0/groups/$project_id/apps/$client_app_id/triggers")
